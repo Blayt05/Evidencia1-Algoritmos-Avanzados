8fabdb651f6abf2bda2f8e8e8cdb0c52b4568017
Israel Booz <A00840268@tec.mx>
2025-09-22 23:43:02 -0600

    Avance Punto 3

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ def leer_archivo(ruta):
             seq.append(line.strip())  # Se quitan saltos de línea y espacios
     return "".join(seq) # Se unen todos los arreglos en uno solo sin espacios
 
+#Funcion que nos ayuda a formatear de manear correcta el nombre de la proteinas y su secuencia en aminoacidos
+def leer_archivo_proteina(ruta):
+    diccionario_proteina = {}
+    secuencia_actual = ""
+    with open(ruta, "r", encoding="utf-8") as f: #Se abre el archivo fasta y se cierra con with
+        for line in f: #Para cada linea del archivo 
+            if line.startswith(">"): # Si empieza con > 
+                nombre_actual = line[1:].strip()  # Se toma el nombre
+            else:
+                secuencia_actual = line.strip()# Se quitan saltos de línea y espacios
+                diccionario_proteina[nombre_actual] = secuencia_actual
+    return diccionario_proteina # Se unen todos los arreglos en uno solo sin espacios
+
 def computeLPSArray(pat, M, lps):
     len = 0
     i = 1
@@ -56,16 +69,87 @@ def KMPSearch(pat, txt, nombre):
 genoma = leer_archivo("archivos/SARS-COV-2-MN908947.3.txt") # Archivo del genoma completo
 
 #Estos serian los patrones
-genM   = leer_archivo("archivos/gen-M.txt") # el gen M  
-genS   = leer_archivo("archivos\gen-S.txt") # el gen S
-genORF = leer_archivo("archivos\gen-ORF1AB.txt") # el gen ORF1ab
-nombre = "genM"
-nombre2 = "genS"
-nombre3 = "genORF"
-KMPSearch(genM,genoma, nombre)
-KMPSearch(genS,genoma,nombre2)
-KMPSearch(genORF,genoma,nombre3)
+# genM   = leer_archivo("archivos/gen-M.txt") # el gen M  
+# genS   = leer_archivo("archivos\gen-S.txt") # el gen S
+# genORF = leer_archivo("archivos\gen-ORF1AB.txt") # el gen ORF1ab
+# nombre = "genM"
+# nombre2 = "genS"
+# nombre3 = "genORF"
+# KMPSearch(genM,genoma, nombre)
+# KMPSearch(genS,genoma,nombre2)
+# KMPSearch(genORF,genoma,nombre3)
 
+
+#Tabla de aminoacidos, aqui vemos las equivalencias de tripletes de nucleotidos a aminoacidos
+codones = {
+    "ATG": "M",
+    "GCT": "A",
+    "GCC": "A",
+    "GCA": "A",
+    "GCG": "A",
+    "CGT": "R",
+    "CGC": "R",
+    "CGA": "R",
+    "CGG": "R",
+    "AGA": "R",
+    "AGG": "R",
+    "AAT": "N",
+    "AAC": "N",
+    "GAT": "D",
+    "GAC": "D",
+    "TGT": "C",
+    "TGC": "C",
+    "CAA": "Q",
+    "CAG": "Q",
+    "GAA": "E",
+    "GAG": "E",
+    "GGT": "G",
+    "GGC": "G",
+    "GGA": "G",
+    "GGG": "G",
+    "CAT": "H",
+    "CAC": "H",
+    "ATT": "I",
+    "ATC": "I",
+    "ATA": "I",
+    "CTT": "L",
+    "CTC": "L",
+    "CTA": "L",
+    "CTG": "L",
+    "TTA": "L",
+    "TTG": "L",
+    "AAA": "K",
+    "AAG": "K",
+    "TTT": "F",
+    "TTC": "F",
+    "CCT": "P",
+    "CCC": "P",
+    "CCA": "P",
+    "CCG": "P",
+    "TCT": "S",
+    "TCC": "S",
+    "TCA": "S",
+    "TCG": "S",
+    "AGT": "S",
+    "AGC": "S",
+    "ACT": "T",
+    "ACC": "T",
+    "ACA": "T",
+    "ACG": "T",
+    "TGG": "W",
+    "TAT": "Y",
+    "TAC": "Y",
+    "GTT": "V",
+    "GTC": "V",
+    "GTA": "V",
+    "GTG": "V",
+    "TAA": "STOP",
+    "TGA": "STOP",
+    "TAG": "STOP"
+}
+
+proteinas = leer_archivo_proteina("archivos/seq-proteins.txt")
+print(proteinas)
 # print(genoma)
 
 
